0b4efe5d28c7246f12806d617ab336e6cea2e2c8
FrancescoLoi <loi.1655748@studenti.uniroma1.it>
2022-10-04 18:37:24 +0200

    aggiustamenti 1

--- addons/corsa/__manifest__.py
+++ addons/corsa/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Corsa",
+    'name': "corsa",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -25,6 +25,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        
         'views/views.xml',
         'views/templates.xml',
     ],
--- addons/corsa/models/pista.py
+++ addons/corsa/models/pista.py
@@ -1,19 +1,17 @@
 from odoo import api, fields, models
 class Pista(models.Model):
-    _name = 'Registro_corse'
-    _description = "Corsa"
+    _name = 'Pista'
+    _description = 'Pista'
     _rec_name = 'ID_pista'
 
-    ID_pista = fields.Char(string='ID_pista', required=True, readonly=True, default='New')
+    ID_pista = fields.Char(string='ID_pista', required=True)
     Nome = fields.Char(string= 'nome')
     Lunghezza = fields.Float(string= 'lunghezza')
-    Tipologia = fields.Selection([('Grande'),('Piccola')], required=True, default='new')
+    Tipologia = fields.Selection([('grande','Grande'),('piccola','Piccola')], required=True)
     
-def create(self, vals):
-    if vals.get('ID_pista','New') == 'New':
-        vals['ID_pista'] = self.env['ir.sequence'].next_by_code('registro_corse.sequence')
-    result = super(Pista, self).create(vals)
-    return result
+
+   
 
 
-    @api.model
+  
+ 
